093113956d95ca36871af824af746d27955b3066
Daniel Small <piktur.io@gmail.com>
2018-09-07 23:37:25 +1000

    🔧 config: enable js class syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   "extends": "eslint:recommended",
   "parserOptions": {
     "ecmaFeatures": {
+      "classes": true,
       "experimentalObjectRestSpread": true,
       "jsx": true
     },

dcbbf793371f4e2b29c95a42d5e20ec802d73b1c
Daniel Small <piktur.io@gmail.com>
2018-09-07 19:48:02 +1000

    🔧 config: add Piktur utils

---
+++ lib/psyche.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Psyche
+
+  extend ::ActiveSupport::Autoload
+
+  # Setup utilities and aliases
+  ::Piktur.install(self, containerize: true)
+
+  class << self
+
+  end
+
+end

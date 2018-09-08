6ccb6e2c0c20dd362ed2b0e39ae1c9897e6cfc03
Daniel Small <piktur.io@gmail.com>
2018-09-08 10:24:21 +1000

    🔧 config: database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,21 @@ require 'piktur/spec/spec_helper'
 Piktur::Spec.init_coverage_reporting!(rails: true)
 
 require 'piktur/spec/rails_helper'
+require 'database_cleaner'
 
-RSpec.configure do |c|
+RSpec.configure do |config|
+  config.prepend_before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    Rails.application.load_seed
+  end
 
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  config.use_transactional_fixtures = false
 end

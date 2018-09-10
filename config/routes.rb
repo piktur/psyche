9c7fd466c383aa343557381668c2b37c847be8c3
Daniel Small <piktur.io@gmail.com>
2018-09-10 18:10:11 +1000

    refactor: route non-existent paths to SPA

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
+
+  get '*path', to: 'application#index'
 end

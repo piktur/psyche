2d30b558efe1db4bfbdb527f723250fe58efdaf7
Daniel Small <piktur.io@gmail.com>
2018-09-10 18:09:52 +1000

    improvement: derive current user Authorization header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,20 @@
 
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
-
   def index
     render template: 'index'
   end
 
   private
 
-    # @return [User]
+    # @return [User, nil]
     def current_user
-      return unless request.headers.key?('Authorization')
+      _, token = request.headers['Authorization']&.split(' ')
+
+      return if token.blank?
 
       ::User.find_by(
-        id: ::Psyche['token_issuer'].call(
-          token: request.headers['Authorization'].split[-1]
-        ).claims[:id]
+        id: ::Psyche['token_issuer'].call(token: token).claims[:id]
       )
     end
 

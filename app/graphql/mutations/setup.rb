31d98b1a2e00aa967f7434b7a099b4e1936ce56b
Daniel Small <piktur.io@gmail.com>
2018-09-10 18:10:00 +1000

    add: factory mutation

---
+++ app/graphql/mutations/setup.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Mutations
+
+  class Setup < ::GraphQL::Schema::RelayClassicMutation
+
+    ROLES = ::Psyche['enum.authorization.roles'].keys
+    private_constant :ROLES
+
+    graphql_name 'Setup'
+    description 'Internal API, exports mock data'
+
+    argument :validFor, ::Integer, required: false
+
+    ROLES.each do |role|
+      field "#{role}_token".to_sym, ::String, null: true
+    end
+
+    def resolve(*)
+      ROLES.each_with_object({}) do |e, a|
+        email = "#{e}@example.com"
+        user = ::User.find_by(email: email) ||
+          ::User.create(email: email, password: 'password')
+        a["#{e}_token"] = ::Psyche['token_issuer'].call(user.to_jwt_claims)
+      end
+    end
+
+  end
+
+end

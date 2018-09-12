94c9683da258bf924e0e8a039aaa71c6eed55588
Daniel Small <piktur.io@gmail.com>
2018-09-12 11:16:17 +1000

    👕 refactor: expose constants

--- app/javascript/src/constants.js
+++ app/javascript/src/constants.js
@@ -1,5 +1,10 @@
 // @flow
 
-export const GRAPHQL_URL: string = process.env.GRAPHQL_URL
-export const AUTH_TOKEN: string = 'viewer_token'
-export const AUTH_ENTITY: string = 'viewer'
+export const GRAPHQL_URL: ?string = process.env.GRAPHQL_URL
+export const AUTH_TOKEN = 'viewer_token'
+export const AUTH_ENTITY = 'viewer'
+export const ROLES: [string, string, string, string] = ['customer', 'clinic', 'clinician', 'admin']
+
+if (!GRAPHQL_URL) {
+  throw Error('GRAPHQL_URL undefined')
+}

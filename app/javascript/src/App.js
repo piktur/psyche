7834df21b0481a4d6d0fb2ac1580b5612b8956f8
Daniel Small <piktur.io@gmail.com>
2018-09-12 15:03:46 +1000

    🔧 config: Router instance

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,88 +1,25 @@
 // @flow
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { QueryRenderer, createFragmentContainer, graphql } from 'react-relay'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import environment from './Environment'
-import Dashboard from './components/Dashboard/Dashboard'
+import * as React from 'react'
+import { createFragmentContainer, graphql } from 'react-relay'
 import Layout from './components/Layout/Layout'
-import Loader from './components/Loader'
-import 'typeface-roboto'
-import ErrorPage from './pages/ErrorPage'
-import IndexPage from './pages/IndexPage'
-import SignIn from './components/SignIn/SignIn'
-import Profile from './components/Profile/Profile'
 
 type Props = {
-  viewer: {
+  isLoading: boolean,
+  viewer: ?{
     role: ?string,
     isAuthenticated: boolean,
-  }
+  },
 }
 
-const App = ({ error, props }: { error: ?Error, props: ?Props }) => {
-  if (error) {
-    return <ErrorPage error={error.message} />
-  }
-
-  if (props) {
-    const { viewer } = props
-
-    return (
-      <BrowserRouter>
-        <Layout classes={{}} viewer={viewer}>
-          <Switch>
-            <Route exact path="/" component={IndexPage} />
-            <Route exact path="/login" component={SignIn} />
-            <Route exact path="/sign-up" component={SignIn} />
-            <Route exact path="/admin" component={Dashboard} />
-            <Route exact path="/customer" component={Dashboard} />
-            <Route exact path="/clinic" component={Dashboard} />
-            <Route exact path="/clinician" component={Dashboard} />
-            <Route exact path="/profile" component={Profile}/>
-            <Route component={ErrorPage} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    )
-  } else {
-    return <Loader />
-  }
+const App = (props: Props): Layout => {
+  return <Layout {...props} isLoading={props.isLoading} />
 }
 
-const AppQuery = graphql`
-  query AppQuery {
-    viewer @__clientField(handle: "viewer") {
-      ...App_viewer @relay(mask: false)
-    }
-  }
-`
-
-createFragmentContainer(App, graphql`
+export default createFragmentContainer(App, graphql`
   fragment App_viewer on Viewer {
     role
     isAuthenticated
     ...Layout_viewer
   }
 `)
-
-const render = () => {
-  const app = document.getElementById('app')
-
-  if (!app) {
-    throw new Error('Root element `#app` missing')
-  }
-
-  ReactDOM.render(
-    <QueryRenderer
-      environment={environment}
-      query={AppQuery}
-      variables={{}}
-      render={App}
-    />,
-    app
-  )
-}
-
-document.addEventListener('DOMContentLoaded', render)
---
+++ app/javascript/src/Router.js
@@ -0,0 +1,91 @@
+// @flow
+
+import * as React from 'react'
+import { BrowserProtocol, queryMiddleware } from 'farce'
+import {
+  createFarceRouter,
+  createRender,
+  makeRouteConfig,
+  Route,
+  matchShape
+} from 'found'
+import { Resolver } from 'found-relay'
+import { graphql } from 'react-relay'
+import environment from './Environment'
+
+import App from './App'
+import IndexPage from './pages/Index'
+import ErrorPage from './pages/Error'
+import Profile from './pages/Profile/Profile'
+import SignIn from './pages/SignIn/SignIn'
+import Dashboard from './components/Dashboard/Dashboard'
+import Loader from './components/Loader'
+
+const appQuery = graphql`
+  query Router_AppQuery {
+    viewer @__clientField(handle: "viewer") {
+      ...App_viewer
+    }
+  }
+`
+
+const signInQuery = graphql`
+  query Router_SignInQuery {
+    viewer {
+      ...SignIn_viewer
+    }
+  }
+`
+
+const profileQuery = graphql`
+  query Router_LoginQuery {
+    viewer {
+      ...Profile_viewer
+    }
+  }
+`
+
+const dashboardQuery = graphql`
+  query Router_DashboardQuery {
+    viewer {
+      ...Dashboard_viewer
+    }
+  }
+`
+
+const Root = ({ match, props }: { match: matchShape, props: Object }): App => {
+  if (!props) {
+    return <Loader />
+  }
+  return <App {...match} {...props} isLoading={!props} />
+}
+
+const routeConfig = makeRouteConfig(
+  <Route
+    path="/"
+    query={appQuery}
+    render={Root}
+  >
+    <Route Component={IndexPage} />
+    <Route path="login" Component={SignIn} query={signInQuery} />
+    <Route path="signup" Component={SignIn} query={signInQuery} />
+    <Route path="profile" Component={Profile} query={profileQuery} />
+    <Route path="admin" Component={Dashboard} query={dashboardQuery} />
+    <Route path="customer" Component={Dashboard} query={dashboardQuery} />
+    <Route path="clinic" Component={Dashboard} query={dashboardQuery} />
+    <Route path="clinician" Component={Dashboard} query={dashboardQuery} />
+  </Route>
+)
+
+const resolver = new Resolver(environment)
+
+const Router = createFarceRouter({
+  historyProtocol: new BrowserProtocol(),
+  historyMiddlewares: [queryMiddleware],
+  routeConfig,
+  render: createRender({
+    renderError: ({ error }) => <ErrorPage error={error} />,
+  }),
+})
+
+export default () => <Router resolver={resolver} />

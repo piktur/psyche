93d8b15df132348ccf3ed5de694e7ad1acee12cf
Daniel Small <piktur.io@gmail.com>
2018-09-12 11:22:30 +1000

    ✅ test: Profile

--- cypress/integration/profile.js
+++ cypress/integration/profile.js
@@ -3,7 +3,7 @@ const f = require('../fixtures')
 describe('Profile', function() {
   const firstName = f.name.firstName()
   const lastName = f.name.lastName()
-  const email = 'customer@example.com'
+  const email = `${firstName}@example.com`
   const password = 'password'
 
   it('visits root; clicks Sign In; acquires token; sees Profile form', () => {
@@ -11,9 +11,10 @@ describe('Profile', function() {
     cy.get('button[aria-label="Sign In"]').click()
     cy.get('input[name=email]').type(email)
     cy.get('input[name=password]').type(password)
-    cy.get('button[type=submit]').click()
-    // cy.wait(200)
-    // cy.get('.MuiListItem', 'Customer').click()
+    cy.get('div[class*=MuiSelect-selectMenu]').click()
+    cy.get('[data-value=customer]').click()
+
+    cy.get('button[name=sign-up]').click()
 
     cy.location('pathname').should('eq', '/profile')
   })
@@ -25,7 +26,6 @@ describe('Profile', function() {
 
   it('fills non-required fields', () => {
     cy.get('input[name=birthday]')
-    cy.get('input[name=role]').value()
   })
 
   it('submits form', () => {

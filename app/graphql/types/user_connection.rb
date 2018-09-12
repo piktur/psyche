b20eb95de7bc760e6e5d96c4ea39a82ffd7e33e4
Daniel Small <piktur.io@gmail.com>
2018-09-12 11:18:52 +1000

    ➕ add: GraphQL types

---
+++ app/graphql/types/user_connection.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+
+  class UserConnection < BaseConnection
+
+    edge_type(UserEdge)
+
+  end
+
+end
---
+++ app/graphql/types/user_edge.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+
+  class UserEdge < BaseEdge
+
+    node_type(UserType)
+
+  end
+
+end

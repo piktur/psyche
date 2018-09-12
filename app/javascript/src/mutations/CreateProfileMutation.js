1ba88dbc46ba922ee1ad4b905ce805d88059aa92
Daniel Small <piktur.io@gmail.com>
2018-09-12 11:16:30 +1000

    ⭐️ new: CreateProfile mutation

--- app/javascript/src/mutations/CreateProfileMutation.js
+++ app/javascript/src/mutations/CreateProfileMutation.js
@@ -3,36 +3,37 @@
 import { commitMutation, graphql } from 'react-relay'
 import environment from '../Environment'
 
-// const mutation = graphql`
-//   mutation CreateProfileMutation($input: Profile!) {
-//     createProfile(input: $input) {
-//       firstName
-//       lastName
-//       birthday
-//       user
-//       errors {
-//         message
-//         path
-//       }
-//     }
-//   }
-// `
+const mutation = graphql`
+  mutation CreateProfileMutation($input: CreateProfileInput!) {
+    createProfile(input: $input) {
+      profile {
+        firstName
+        lastName
+        birthday
+      }
+      errors {
+        message
+        path
+      }
+    }
+  }
+`
 
-// export default (profile: Object, cb: Function) => {
-//   const variables = {
-//     input: {
-//       ...profile,
-//       clientMutationId: '',
-//     }
-//   }
+export default (profile: Object, cb: Function) => {
+  const variables = {
+    input: {
+      ...profile,
+      clientMutationId: '',
+    }
+  }
 
-//   commitMutation(
-//     environment,
-//     {
-//       mutation,
-//       variables,
-//       onCompleted: (data) => { cb(data.createProfile) },
-//       onError: (err) => { console.error(err) }
-//     }
-//   )
-// }
+  commitMutation(
+    environment,
+    {
+      mutation,
+      variables,
+      onCompleted: (data) => { cb(data.createProfile) },
+      onError: (err) => { console.error(err) }
+    }
+  )
+}

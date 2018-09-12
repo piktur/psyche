c4f52c3c5fdce28d65e238ec3184fbb82700e64b
Daniel Small <piktur.io@gmail.com>
2018-09-12 11:18:18 +1000

    👕 refactor: App

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -6,79 +6,83 @@ import { QueryRenderer, createFragmentContainer, graphql } from 'react-relay'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import environment from './Environment'
 import Dashboard from './components/Dashboard/Dashboard'
-import Fade from '@material-ui/core/Fade'
 import Layout from './components/Layout/Layout'
-import IndexPage from './pages/IndexPage'
 import Loader from './components/Loader'
+import 'typeface-roboto'
+import ErrorPage from './pages/ErrorPage'
+import IndexPage from './pages/IndexPage'
 import SignIn from './components/SignIn/SignIn'
 import Profile from './components/Profile/Profile'
-import Typography from '@material-ui/core/Typography'
-import 'typeface-roboto'
 
 type Props = {
   viewer: {
-    role: number,
-    profile: {
-      firstName: string,
-      lastName: string,
-    }
+    role: ?string,
+    isAuthenticated: boolean,
   }
 }
 
-class App extends React.Component<Props> {
-  render() {
-    const { viewer } = this.props
+const App = ({ error, props }: { error: ?Error, props: ?Props }) => {
+  if (error) {
+    return <ErrorPage error={error.message} />
+  }
+
+  if (props) {
+    const { viewer } = props
 
     return (
       <BrowserRouter>
         <Layout classes={{}} viewer={viewer}>
           <Switch>
-            <Route exact path="/" component={viewer ? Dashboard : IndexPage} />
-            <Route exact path="/login" component={SignIn}/>
+            <Route exact path="/" component={IndexPage} />
+            <Route exact path="/login" component={SignIn} />
+            <Route exact path="/sign-up" component={SignIn} />
+            <Route exact path="/admin" component={Dashboard} />
+            <Route exact path="/customer" component={Dashboard} />
+            <Route exact path="/clinic" component={Dashboard} />
+            <Route exact path="/clinician" component={Dashboard} />
             <Route exact path="/profile" component={Profile}/>
+            <Route component={ErrorPage} />
           </Switch>
         </Layout>
       </BrowserRouter>
     )
+  } else {
+    return <Loader />
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+const AppQuery = graphql`
+  query AppQuery {
+    viewer @__clientField(handle: "viewer") {
+      ...App_viewer @relay(mask: false)
+    }
+  }
+`
+
+createFragmentContainer(App, graphql`
+  fragment App_viewer on Viewer {
+    role
+    isAuthenticated
+    ...Layout_viewer
+  }
+`)
+
+const render = () => {
+  const app = document.getElementById('app')
+
+  if (!app) {
+    throw new Error('Root element `#app` missing')
+  }
+
   ReactDOM.render(
     <QueryRenderer
       environment={environment}
-      query={graphql`
-        query AppQuery {
-          viewer {
-            ...App_viewer
-          }
-        }
-      `}
+      query={AppQuery}
       variables={{}}
-      render={({ error, props }) => {
-        if (error) {
-          return <Typography variant="display4" gutterBottom>Error!</Typography>
-        }
-
-        if (props) {
-          return <App viewer={props.viewer}/>
-        } else {
-          return <Loader />
-        }
-
-      }}
+      render={App}
     />,
-    document.getElementById('app')
+    app
   )
-})
+}
 
-createFragmentContainer(App, graphql`
-  fragment App_viewer on User {
-    role
-    profile {
-      firstName
-      lastName
-    }
-    ...Nav_viewer
-  }
-`)
+document.addEventListener('DOMContentLoaded', render)
